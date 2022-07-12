efb40ea55d1c964f2ec09c32d3adb6038a0581a4
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2022-07-12 23:02:28 +0200

    Create SecondsPatern.lua

---
+++ SecondsPatern.lua
@@ -0,0 +1,11 @@
+local patternValues = {
+		["s"] = 1, -- seconds
+		["m"] = 60, -- minutes
+		["h"] = 3600, -- hours
+		["d"] = 86400, -- days
+		["w"] = 604800, -- weeks
+		["mo"] = 2628000, -- months
+		["y"] = 31540000, -- years
+	}
+  
+  return patternValues
