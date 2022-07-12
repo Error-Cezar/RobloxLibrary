ddc3a2150ce77c48d5d2badd0c5cc24fdec7bec1
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2022-07-12 23:00:21 +0200

    Create GetMonth.lua

---
+++ GetMonth.lua
@@ -0,0 +1,21 @@
+
+function GetMonth(M: number)
+	local Month = {
+		[1]  = "January",
+		[2]  = "February",
+		[3]  = "March",
+		[4]  = "April",
+		[5]  = "May",
+		[6]  = "June",
+		[7]  = "July",
+		[8]  = "August",
+		[9]  = "September",
+		[10] = "October",
+		[11] = "November",
+		[12] = "December",
+	}
+	if Month[M] then
+		return Month[M]
+	end
+	return nil
+end
