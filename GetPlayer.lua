692a43f48df529124311c442be7e20d640e432bc
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2022-07-21 16:08:42 +0200

    Update GetPlayer.lua

--- GetPlayer.lua
+++ GetPlayer.lua
@@ -1,7 +1,7 @@
 function GetPlayer(player: string)
    for _, User in pairs(game:GetService("Players"):GetPlayers()) do
-		   if string.sub(string.lower(User.Name), 0, string.len(player)) == string.lower(player) then
-                       player = User.Name
-		   end
+          if string.sub(string.lower(User.Name), 0, string.len(player)) == string.lower(player) then
+                 player = User.Name
+          end
    end
 end
